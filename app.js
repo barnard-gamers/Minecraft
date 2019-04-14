4b29f2dd1960b3be402dbc89fe50932035688874
hxdegawa <hxdegawa@gmail.com>
2019-04-14 20:29:36 +0900

    Removed few extensions

--- app.js
+++ app.js
@@ -8,12 +8,52 @@ const server = new ScriptServer({
       port: '25575',
       password: 'aMUuENdQmQj6Z0wRgxUQ'
     }
+  },
+  essentials: {
+    motd: {
+      enabled: true,
+      first: 'Welcome to the Barnard, ${player}!',
+      text: 'Welcome back ${player}!'
+    },
+    starterKit: {
+      enabled: true,
+      items: [
+        'iron_pickaxe',
+        'iron_shovel',
+        'iron_axe',
+        'iron_sword',
+        'bed',
+        'bread 32',
+      ]
+    },
+    home: {
+      enabled: true,
+      amount: 3
+    },
+    spawn: true,
+    warp: {
+      enabled: true,
+      opOnly: true
+    },
+    tpa: true,
+    back: true,
+    day: {
+      enabled: true,
+      percent: 50
+    },
+    night: {
+      enabled: true,
+      percent: 50
+    },
+    weather: {
+      enabled: true,
+      percent: 50
+    }
   }
 });
 
 async function bootServer () {
   await server.use(require('scriptserver-event'));
-  await server.use(require('scriptserver-essentials'));
   await server.start();
 }
 
